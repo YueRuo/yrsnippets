8a31b1911db0c73b2b3ce99c0880cb6a79dd13d6
王晓宇 <YueRuo@ouxiaoyumatoMac-mini.local>
2014-04-28 21:34:02 +0800

    Imported user code snippets

---
+++ yrcodeseprate.m
@@ -0,0 +1,17 @@
+// YRCodeSeprate
+// To help make the code looks streakiness
+//
+// IDECodeSnippetCompletionPrefix: yrcodeseprate
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: D5583946-7469-4230-8029-A9B4F2EF7BD2
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+
+    //******************************
+    //*-begin-- <#Description#>
+    //******************************
+    
+    //----------------------------
+    //-end-- <#Description#>
+    //---------------------------
---
+++ yrinitclass.m
@@ -0,0 +1,15 @@
+// YRInitClass
+// Init class
+//
+// IDECodeSnippetCompletionPrefix: yrinitclass
+// IDECodeSnippetCompletionScopes: [ClassImplementation]
+// IDECodeSnippetIdentifier: 67396617-A534-426F-9672-F2CDED942F1D
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+-(id)init{
+    if (self=[super init]) {
+        
+    }
+    return self;
+}
